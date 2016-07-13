776739e5661e7ef03c15c32bf5424faa28cb9475
Amit Chhabra <chhabraamit.nsit@gmail.com>
2016-07-13 19:06:47 +0000

    Add common words for the languages

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -1,9 +1,43 @@
 """This file contains the definition of all languages supported by the program."""
 
 LANGUAGES = [
-    {
-        'name': 'Spanish',
-        'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
-    },
-    #  include your extra languages here
-]
+            {
+                'name': 'Spanish',
+                'common_words': [
+                    'el', 'la', 'de', 'que', 'y', 'a', 'en', 'un', 'ser', 'se',
+                    'no', 'haber', 'por', 'con', 'su', 'para', 'como', 'estar',
+                    'tener', 'le', 'lo', 'lo', 'todo', 'pero', 'más', 'hacer',
+                    'o', 'poder', 'decir', 'este', 'ir', 'otro', 'ese', 'la',
+                    'si', 'me', 'ya', 'ver', 'porque', 'dar', 'cuando', 'él',
+                    'muy', 'sin', 'vez', 'mucho', 'saber', 'qué', 'sobre',
+                    'mi', 'alguno', 'mismo', 'yo', 'también', 'hasta'
+                ]
+            },
+            {
+                'name': 'German',
+                'common_words': [
+                    'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
+                    'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
+                    'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+                    'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+                    'dich', 'war', 'haben', 'für', 'an', 'habe', 'da', 'nein',
+                    'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+                    'einen', 'kann', 'dem'
+                ]
+            },
+            {
+                'name': 'English',
+                'common_words': [
+                    'the', 'he', 'at', 'but', 'there',
+                    'of', 'was', 'be', 'not', 'use',
+                    'and', 'for', 'this', 'what', 'an',
+                    'a', 'on', 'have', 'all', 'each',
+                    'to', 'are', 'from', 'were', 'which',
+                    'in', 'as', 'or', 'we', 'she',
+                    'is', 'with', 'ine', 'when', 'do',
+                    'you', 'his', 'had', 'your', 'how',
+                    'that', 'they', 'by', 'can', 'their',
+                    'it', 'I', 'word', 'said', 'if'
+                    ]
+            }
+        ]
