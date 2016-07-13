a597aedb2f78265cc25f6d410addfd533cc66ecb
Amit Chhabra <chhabraamit.nsit@gmail.com>
2016-07-13 21:50:15 +0000

    Add docstring

--- language_detector/main.py
+++ language_detector/main.py
@@ -8,19 +8,20 @@ def detect_language(text, languages=LANGUAGES):
 	max_common_words = 0
 	for language in languages:
 		common_words = language["common_words"]
-		common_words_count = getCount(text, common_words)
-		print(language["name"], common_words_count)
+		common_words_count = get_common_words_count(text, common_words)
 		if common_words_count > max_common_words:
 			max_common_words = common_words_count
 			ans = language["name"]
 	return ans
 
-def getCount(text, words):
-	ans = 0
-	lower_text = text.lower()
-	set_text = set(lower_text.split(" "))
-	for word in words:
-	    if word in set_text:
-	        ans += 1
-	return ans/(len(set_text))
+def get_common_words_count(text, words):
+    """ returns number of words in list 'words' 
+    are in string 'text' and normalize them"""
+    ans = 0
+    lower_text = text.lower()
+    set_text = set(lower_text.split(" "))
+    for word in words:
+        if word in set_text:
+            ans += 1
+    return ans/(len(set_text))
 
