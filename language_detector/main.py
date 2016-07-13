d8c1de937961b5682644cb89c466d7b879bd7cff
Amit Chhabra <chhabraamit.nsit@gmail.com>
2016-07-13 19:06:18 +0000

    Add language detection code

--- language_detector/main.py
+++ language_detector/main.py
@@ -4,6 +4,19 @@ from .languages import LANGUAGES
 
 
 def detect_language(text, languages=LANGUAGES):
-    """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+	"""Returns the detected language of given text."""
+	max_common_words = 0
+	for language in languages:
+		common_words = language["common_words"]
+		common_words_count = getCount(text, common_words)
+		if common_words_count > max_common_words:
+			max_common_words = common_words_count
+			ans = language["name"]
+	return ans
+
+def getCount(text, words):
+	ans = 0
+	for word in words:
+		if word in text:
+			ans+= 1
+	return ans
